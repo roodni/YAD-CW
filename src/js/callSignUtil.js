272f85e2c7b628665a878c91156871da98389de4
rodoni <41773436+roodni@users.noreply.github.com>
2019-07-11 14:23:34 +0900

    prefix生成を関数に分離

--- src/js/callSignUtil.js
+++ src/js/callSignUtil.js
@@ -1,35 +1,5 @@
-export function generate() {
-    const charAlphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const charNumbers = '0123456789';
-
-    const charAlphabetsWithoutQ = charAlphabets.replace('Q', '');
-    const charNumbersWithout01 = charNumbers.replace('01', '');
-
-    let firstChar = createCharRnd(charAlphabetsWithoutQ + charNumbersWithout01);
-    let secondChar = createCharRnd(charAlphabets + ((charNumbers.includes(firstChar)) ? '' : charNumbersWithout01));
-
-    let prefix = firstChar + secondChar;
-    let number = createCharRnd(charNumbers);
-    let suffix = createStrRnd(charAlphabets, 3 - (randInt(50) == 0 ? 1 : 0));
-    
-    let portable = '';
-    if (randInt(7) == 0) {
-        portable = '/' + randInt(0, 10);
-    }
-
-    return prefix + number + suffix + portable;
-}
-export function highlightDiff(answer, input, highlightCssClass) {
-    let ret = '';
-    for (let i = 0; i < input.length; i++) {
-        let c = input[i];
-        if (answer[i] !== c) {
-            c = `<span class="${highlightCssClass}">${c}</span>`;
-        }
-        ret += c;
-    }
-    return ret;
-}
+const charAlphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const charNumbers = '0123456789';
 
 function randInt(min, max) {
     if (max === undefined) {
@@ -49,4 +19,39 @@ function createStrRnd(charSet, length) {
 
 function createCharRnd(charSet) {
     return charSet[randInt(0, charSet.length)];
+}
+
+function generateWorldPrefix() {
+    const charAlphabetsWithoutQ = charAlphabets.replace('Q', '');
+    const charNumbersWithout01 = charNumbers.replace('01', '');
+
+    let firstChar = createCharRnd(charAlphabetsWithoutQ + charNumbersWithout01);
+    let secondChar = createCharRnd(charAlphabets + ((charNumbers.includes(firstChar)) ? '' : charNumbersWithout01));
+    let number = createCharRnd(charNumbers);
+
+    return firstChar + secondChar + number;
+}
+export function generate() {
+    let prefix;
+
+    prefix = generateWorldPrefix();
+    let suffix = createStrRnd(charAlphabets, 3 - (randInt(50) == 0 ? 1 : 0));
+    
+    let portable = '';
+    if (randInt(7) == 0) {
+        portable = '/' + randInt(0, 10);
+    }
+
+    return prefix + suffix + portable;
+}
+export function highlightDiff(answer, input, highlightCssClass) {
+    let ret = '';
+    for (let i = 0; i < input.length; i++) {
+        let c = input[i];
+        if (answer[i] !== c) {
+            c = `<span class="${highlightCssClass}">${c}</span>`;
+        }
+        ret += c;
+    }
+    return ret;
 }
